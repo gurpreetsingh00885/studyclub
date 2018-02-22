af0c92ded8289b16260b1bcad80828b24162e48b
Gurpreet Singh <gurpreetsingh00885@gmail.com>
2018-02-23 04:54:10 +0530

    Add video search page

--- mainApp/views.py
+++ mainApp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django import views
+from mainApp.forms import VidOpenForm
 # Create your views here.
 
 
@@ -8,3 +9,11 @@ class HomePageView(views.View):
 		return render(request, 'home/index.html' , {})
 
 home_page = HomePageView.as_view()
+
+class VideoSearchView(views.View):
+	def get(self, request, *args, **kwargs):
+		form = VidOpenForm()
+
+		return render(request, 'vidsearch/index.html' , {"form": form})
+
+vid_search_page = VideoSearchView.as_view()
