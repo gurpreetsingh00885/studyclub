180a8eb1ca0df164a70383118b3d408ba7b8e3f5
Gurpreet Singh <gurpreetsingh00885@gmail.com>
2018-02-23 07:09:58 +0530

    Add Video cc get logic

--- mainApp/views.py
+++ mainApp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django import views
 from mainApp.forms import VidOpenForm
+from embed_video.backends import detect_backend
 # Create your views here.
 
 
@@ -13,7 +14,11 @@ home_page = HomePageView.as_view()
 class VideoSearchView(views.View):
 	def get(self, request, *args, **kwargs):
 		form = VidOpenForm()
-
 		return render(request, 'vidsearch/index.html' , {"form": form})
+
+	def post(self, request, *args, **kwargs):
+		video = detect_backend(request.POST['url'])
+		cc_url = "https://www.youtube.com/api/timedtext?lang=en&v=%s" %(video.get_code())
+		return render(request, 'vidsearch/view.html', {"my_video": video, "cc_url": cc_url})
 
 vid_search_page = VideoSearchView.as_view()
